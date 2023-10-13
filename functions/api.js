3bc31800cb3755b36f4bb52ec72a55d6e37d85ad
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-10-14 01:11:46 +0300

    Use Async Logic

--- functions/api.js
+++ functions/api.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const serverless = require("serverless-http");
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 
@@ -11,7 +12,7 @@ const router = express.Router();
 // Limit requests from the same IP
 const limiter = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 24 hours
-    max: 10, // Maximum requests per windowMs
+    max: 20, // Maximum requests per windowMs
 });
 
 app.use(limiter);
@@ -26,41 +27,41 @@ app.use(
 );
 app.use(express.json());
 
-router.post("/send-email", (req, res) => {
+router.post("/send-email", async (req, res) => {
     const { name, email, message } = req.body;
 
-    // Create a transporter using SMTP configuration
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-        },
-    });
+    try {
+        // Create a transporter using SMTP configuration
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: true,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
 
-    // Prepare the email content
-    const mailOptions = {
-        from: `"${name}" <${email}>`,
-        to: process.env.SMTP_USER,
-        replyTo: email,
-        subject: "New message from your website",
-        text: `From: ${name} \nEmail:<${email}>\n\nMessage:${message}`,
-    };
+        // Prepare the email content
+        const mailOptions = {
+            from: `"${name}" <${email}>`,
+            to: process.env.SMTP_USER,
+            replyTo: email,
+            subject: "New message from your website",
+            text: `From: ${name} \nEmail:<${email}>\n\nMessage:${message}`,
+        };
 
-    // Send the email
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error("Error sending email:", error);
-            res.status(500).json({
-                error: "An error occurred while sending the email",
-            });
-        } else {
-            console.log("Email sent:", info.response);
-            res.json({ message: "Email sent successfully" });
-        }
-    });
+        // Send the email using await
+        const info = await transporter.sendMail(mailOptions);
+
+        console.log("Email sent:", info.response);
+        res.json({ message: "Email sent successfully" });
+    } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).json({
+            error: "An error occurred while sending the email",
+        });
+    }
 });
 
 app.use(`/.netlify/functions/api`, router);
--- server.js
+++ server.js
@@ -3,55 +3,69 @@ const serverless = require("serverless-http");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 const router = express.Router();
 
+// Limit requests from the same IP
+const limiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 24 hours
+    max: 10, // Maximum requests per windowMs
+});
+
+app.use(limiter);
+
 app.use(
     cors({
-        origin: ["https://markuss-portfolio.netlify.app/"],
+        origin: [
+            "https://markuss-portfolio.netlify.app",
+            "https://main--markuss-portfolio.netlify.app",
+            "http://localhost:3000",
+        ],
     })
 );
 app.use(express.json());
 
-router.post("/send-email", (req, res) => {
+router.post("/send-email", async (req, res) => {
     const { name, email, message } = req.body;
 
-    // Create a transporter using SMTP configuration
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: true,
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS,
-        },
-    });
+    try {
+        // Create a transporter using SMTP configuration
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: true,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
 
-    // Prepare the email content
-    const mailOptions = {
-        from: `"${name}" <${email}>`,
-        to: process.env.SMTP_USER,
-        replyTo: email,
-        subject: "New message from your website",
-        text: `From: ${name} \nEmail:<${email}>\n\nMessage:${message}`,
-    };
+        // Prepare the email content
+        const mailOptions = {
+            from: `"${name}" <${email}>`,
+            to: process.env.SMTP_USER,
+            replyTo: email,
+            subject: "New message from your website",
+            text: `From: ${name} \nEmail:<${email}>\n\nMessage:${message}`,
+        };
 
-    // Send the email
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error("Error sending email:", error);
-            res.status(500).json({
-                error: "An error occurred while sending the email",
-            });
-        } else {
-            console.log("Email sent:", info.response);
-            res.json({ message: "Email sent successfully" });
-        }
-    });
+        // Send the email using await
+        const info = await transporter.sendMail(mailOptions);
+
+        console.log("Email sent:", info.response);
+        res.json({ message: "Email sent successfully" });
+    } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).json({
+            error: "An error occurred while sending the email",
+        });
+    }
 });
 
 app.use(`/.netlify/functions/api`, router);
 
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(8080, () => {
+    console.log(`Server is running on port 8080`);
+});
